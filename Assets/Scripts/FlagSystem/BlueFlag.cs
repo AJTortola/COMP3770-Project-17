3856c78629510032aa79a6de506c439aa997b755
Anthony <tortolaa@uwindsor.ca>
2021-12-22 21:54:25 -0500

    Capture the flag System

---
+++ Assets/Scripts/FlagSystem/BlueFlag.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class BlueFlag : MonoBehaviour
+{
+    public bool capturedBlueFlag;
+    public GameObject redTeamFlag;
+    RedFlag redFlag;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        redFlag = redTeamFlag.GetComponent<RedFlag>();
+        gameObject.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "RedPlayer" when capturedBlueFlag == false && gameObject.CompareTag("BlueFlag"): // when Red Player touches flag they steal it
+                capturedBlueFlag = true;
+                gameObject.SetActive(false);
+                break;
+            case "BluePlayer" when redFlag.capturedRedFlag && gameObject.CompareTag("BlueFlag"): // when Blue Player returns to the abse with a red flag
+                redFlag.capturedRedFlag = false;
+                redFlag.gameObject.SetActive(true);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/FlagSystem/RedFlag.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class RedFlag : MonoBehaviour
+{
+    public bool capturedRedFlag = false; // did the blue team capture the read team's flag
+    public GameObject blueTeamFlag; // Reference BlueTeamFlag
+    BlueFlag blueFlag; // references BlueTeamFlag
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blueFlag = blueTeamFlag.GetComponent<BlueFlag>(); // set to blueTeamFlag prefab
+        gameObject.SetActive(true); // set child Model to Active at the start of the game
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag) // switch statement when someone colldes with the redFlag
+        {
+            case "BluePlayer" when capturedRedFlag == false && gameObject.CompareTag("RedFlag"): // when Blue Player touches flag they steal it
+                capturedRedFlag = true; // player captured the flag
+                gameObject.SetActive(false); // Reference RedTeamFlag to turn on and off when it's stolen by blue team
+                break;
+            case "RedPlayer" when blueFlag.capturedBlueFlag && gameObject.CompareTag("RedFlag"): // when Red Player returns to the base with a blue flag
+                blueFlag.capturedBlueFlag = false;
+                blueFlag.gameObject.SetActive(true);
+                break;
+        }
+    }
+}
